fbfa134414b70ab1fa5eb3aaf33e8967e68df078
subham saurabh <saurabhsubahm113@gmail.com>
2021-02-21 18:18:58 +0530

    added routing

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <h1>
+                About: This application is use to track the issues raised.
+            </h1>
+        )
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import AllIssuePage from "./components/AllIssuePage";
+import About from "./About";
 import './App.css';
-
+import { Switch, Link, Route } from 'react-router-dom'
 function App() {
+
+  const linkStyle = { margin: "10px", display: "inline-block" }
+  const container = { margin: "auto", width: "80%" }
   return (
-    <div className="App">
-      <AllIssuePage />
+    <div className="App" style={container}>
+      <Link style={linkStyle} to="/">About</Link>
+      <Link style={linkStyle} to="/issues">Issues</Link>
+      <Switch>
+        <Route path="/" exact component={About} />
+        <Route path="/issues" exact component={AllIssuePage} />
+      </Switch>
     </div>
   );
 }
